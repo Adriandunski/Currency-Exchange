7d4aa77cdff09a4e0986d50ffccb3477913817ea
Adrian <mrduniak@icloud.com>
2019-05-10 20:11:18 +0200

    creature project

---
+++ src/classes/Main.java
@@ -0,0 +1,27 @@
+package classes;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi in Cantor.");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("../fxmls/LoginWindow.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Login Cantor");
+        primaryStage.show();
+    }
+}
