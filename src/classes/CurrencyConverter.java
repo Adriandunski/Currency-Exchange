4a065d3016f040c0e91898eeb0e8868b09020d78
Adrian <mrduniak@icloud.com>
2019-05-11 20:39:14 +0200

    Controllers...

---
+++ src/classes/CurrencyConverter.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class CurrencyConverter {
+
+    public static double convert(Currence from, Currence to) {
+
+
+
+        return 0;
+    }
+}
--- src/controllers/PersonMenuController.java
+++ src/controllers/PersonMenuController.java
@@ -3,9 +3,12 @@ package controllers;
 import classes.Currence;
 import classes.DbConnection.DbHandler;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
@@ -21,10 +24,46 @@ import java.util.ResourceBundle;
 public class PersonMenuController implements Initializable {
 
     @FXML
-    private JFXButton test;
+    private JFXButton buttonCheck;
 
     @FXML
     private GridPane grindPaneCurrence;
+
+    @FXML
+    private ChoiceBox<Currence> choiceBoxA;
+
+    @FXML
+    private ChoiceBox<Currence> choiceBoxB;
+
+    @FXML
+    private JFXTextField fieldCheck;
+
+    @FXML
+    private JFXButton buttonLogOut;
+
+    @FXML
+    private Label labelDeposit;
+
+    @FXML
+    private Label labelQuantity;
+
+    @FXML
+    private Label labelExchangeA;
+
+    @FXML
+    private Label labelExchangeB;
+
+    @FXML
+    private Label labelRecieveA;
+
+    @FXML
+    private Label labelReciveB;
+
+    @FXML
+    private JFXButton buttonCheckOut;
+
+    @FXML
+    private JFXButton buttonCancel;
 
     private Map<Currence, Double> funds = new HashMap<>();
     private Connection connection = new DbHandler().getConnection();
@@ -37,15 +76,59 @@ public class PersonMenuController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
         loadFunds();
+
+        choiceBoxA.addEventFilter(Event.ANY, x -> checkConditions());
+        choiceBoxB.addEventFilter(Event.ANY, x -> checkConditions());
+        fieldCheck.addEventFilter(Event.ANY, x -> {
+            checkConditions();
+
+            if (checkNumbers() && choiceBoxA.getValue() != null) {
+                labelQuantity.setText(String.format("%.2f %s",
+                        Double.parseDouble(fieldCheck.getText()), choiceBoxA.getValue().getCurrence()));
+            }
+        });
+
     }
 
     @FXML
-    void test(ActionEvent event) {
+    void cancelExchange(ActionEvent event) {
+
+        buttonCheck.setVisible(true);
+        buttonCheckOut.setVisible(false);
+        buttonCancel.setVisible(false);
+
+        labelExchangeA.setVisible(false);
+        labelExchangeB.setVisible(false);
+        labelRecieveA.setVisible(false);
+        labelReciveB.setVisible(false);
+    }
+
+    @FXML
+    void checkCurrence(ActionEvent event) {
+
+        buttonCheck.setVisible(false);
+        buttonCheckOut.setVisible(true);
+        buttonCancel.setVisible(true);
+
+        labelExchangeA.setVisible(true);
+        labelExchangeB.setVisible(true);
+        labelRecieveA.setVisible(true);
+        labelReciveB.setVisible(true);
 
     }
 
+    @FXML
+    void checkOutExchange(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOut(ActionEvent event) {
+
+    }
 
     private void addFund() {
+
     }
 
     private void loadFunds() {
@@ -68,6 +151,7 @@ public class PersonMenuController implements Initializable {
             }
 
             System.out.println(funds);
+            addBoxesToChoiceBox();
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -83,6 +167,46 @@ public class PersonMenuController implements Initializable {
         grindPaneCurrence.add(label2, 1, indexOfRow);
 
         indexOfRow++;
+    }
+
+    private void addBoxesToChoiceBox() {
+
+        for (Map.Entry<Currence, Double> entries : funds.entrySet()) {
+
+            if (entries.getValue() > 0) {
+                choiceBoxA.getItems().add(entries.getKey());
+            }
+
+            choiceBoxB.getItems().add(entries.getKey());
+        }
+    }
+
+    //Method check conditions to setDisable(false), two choises box must be selected and one textField.
+    private void checkConditions() {
+
+            if (choiceBoxA.getValue() != null && choiceBoxB.getValue() != null && checkNumbers()) {
+
+                if(!choiceBoxA.getValue().equals(choiceBoxB.getValue()) && fieldCheck.getText().length() > 0) {
+                    buttonCheck.setDisable(false);
+                } else{
+                    buttonCheck.setDisable(true);
+                }
+
+            } else {
+                buttonCheck.setDisable(true);
+            }
+    }
+
+    private boolean checkNumbers() {
+
+        try {
+            double temp = Double.parseDouble(fieldCheck.getText());
+        } catch (NumberFormatException e) {
+            labelQuantity.setText("");
+            return false;
+        }
+
+        return true;
     }
 
     public void setInstanceLoginMenu(LoginWindowController instance) {
